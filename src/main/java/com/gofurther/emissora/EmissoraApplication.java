ca9ab818a962547d5d7cf48073b5fef24bf0d40d
Jonathan Anthony <jonataanthony@gmail.com>
2020-10-27 19:39:11 -0300

    Esqueleto do projeto

---
+++ src/main/java/com/gofurther/emissora/EmissoraApplication.java
@@ -0,0 +1,13 @@
+package com.gofurther.emissora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmissoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmissoraApplication.class, args);
+	}
+
+}
